cf1fbc071b29fe9401d46ef41f38ab9b91437e6d
TheKooperDon <valodinejr@gmail.com>
2023-08-23 10:01:42 -0600

    lol

--- WEB TEMPLATE/script.js
+++ WEB TEMPLATE/script.js
@@ -1,13 +1,15 @@
-// Get the ul element where items will be added
-const itemList = document.getElementById("item-list");
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the ul element where items will be added
+  const itemList = document.getElementById("item-list");
 
-// Example data for the loop
-const itemsData = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"];
+  // Example data for the loop
+  const itemsData = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"];
 
-// Loop through the itemsData array and create list items
-for (let i = 0; i < itemsData.length; i++) {
-  const item = document.createElement("li");
-  item.classList.add("item");
-  item.textContent = itemsData[i];
-  itemList.appendChild(item);
-}
+  // Loop through the itemsData array and create list items
+  for (let i = 0; i < itemsData.length; i++) {
+    const item = document.createElement("li");
+    item.classList.add("item");
+    item.textContent = itemsData[i];
+    itemList.appendChild(item);
+  }
+});

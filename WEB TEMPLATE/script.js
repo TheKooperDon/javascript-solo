cd4811f78400dd10353600fb59267db37efc414e
TheKooperDon <valodinejr@gmail.com>
2023-08-22 21:13:05 -0600

    Testing the chatgpt

---
+++ WEB TEMPLATE/script.js
@@ -0,0 +1,13 @@
+// Get the ul element where items will be added
+const itemList = document.getElementById("item-list");
+
+// Example data for the loop
+const itemsData = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"];
+
+// Loop through the itemsData array and create list items
+for (let i = 0; i < itemsData.length; i++) {
+  const item = document.createElement("li");
+  item.classList.add("item");
+  item.textContent = itemsData[i];
+  itemList.appendChild(item);
+}
